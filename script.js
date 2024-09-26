07060acaf7675a971235b7a6c5adbb49c0830138
Abhishekpatil2000 <115855289+Abhishekpatil2000@users.noreply.github.com>
2024-09-26 14:46:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let submission= new Promise((Resolve,Reject){
+	if (Age>18) {
+		Resolve(Name);
+	}
+	else{
+		Reject(Name);
+	}
+});
+submission.then(setTimeout(function(Name) {
+	console.log(`Welcome,${Name}. You can vote.`)
+}),4000)
+.catch(setTimeout(function(Name) {
+	console.log(`Oh sorry $`)
+}))
