10985773a43bb858f5a1f8fac5739abc8775957a
Abhishekpatil2000 <115855289+Abhishekpatil2000@users.noreply.github.com>
2024-10-02 06:40:51 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-//your JS code here. If required.
 document.getElementById('btn').addEventListener('click',function (p) {
 	p.preventDefault();
 
- let age= document.getElementById('age').value;
- let name= document.getElementById('name').value;
-
+	let age= document.getElementById('age').value;
+	let name= document.getElementById('name').value;
 	
-let submission= new Promise((resolve,reject)=> {
-	if (age>=18) {
-		Resolve(name);
-	}
-	else{
-		Reject(name);
-	}
-});
-submission.then( (name)=> {
-	setTimeout(()=> {
-		alert(`Welcome,${name}. You can vote.`);
-},4000);
-})
-.catch( (name)=>  {
-	setTimeout(()=> { 
-	alert(`Oh sorry ${name}. You aren't old enough.`);
-},4000);
-});
+	let submission= new Promise((resolve,reject)=> {
+		if (age>=18) {
+			resolve(name);
+		}
+		else{
+			reject(name);
+		}
+	});
+	
+	submission.then( (name)=> {
+		setTimeout(()=> {
+			alert(`Welcome,${name}. You can vote.`);
+		},4000);
+	})
+	.catch( (name)=>  {
+		setTimeout(()=> { 
+			alert(`Oh sorry ${name}. You aren't old enough.`);
+		},4000);
+	});
 });
